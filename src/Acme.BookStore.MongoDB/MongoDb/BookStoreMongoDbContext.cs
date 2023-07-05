287e6bfaa1c127cee8d8420b4db38b6161c3b517
fakeblackspectre <fakeblackspectre@protonmail.com>
2023-07-05 17:44:56 +0100

    init

---
+++ src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Acme.BookStore.Books;
+using MongoDB.Driver;
+using Volo.Abp.Data;
+using Volo.Abp.MongoDB;
+
+namespace Acme.BookStore.MongoDB;
+
+[ConnectionStringName("Default")]
+public class BookStoreMongoDbContext : AbpMongoDbContext
+{
+    /* Add mongo collections here. Example:
+     * public IMongoCollection<Question> Questions => Collection<Question>();
+     */
+    public IMongoCollection<Book> Books => Collection<Book>();
+
+    protected override void CreateModel(IMongoModelBuilder modelBuilder)
+    {
+        base.CreateModel(modelBuilder);
+
+        //modelBuilder.Entity<YourEntity>(b =>
+        //{
+        //    //...
+        //});
+    }
+}
